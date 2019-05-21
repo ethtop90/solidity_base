fb083c5edcc5f166ce710c9bceadd7d75c55f6f4
Bhargava Shastry <bhargava.shastry@ethereum.org>
2019-05-21 12:37:57 +0200

    Add solidity wrapper and a fuzzer that makes use of it

---
+++ test/tools/ossfuzz/abiV2Fuzzer.cpp
@@ -0,0 +1,57 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <fstream>
+#include "abiV2FuzzerCommon.h"
+
+using namespace dev::test::abiv2fuzzer;
+using namespace std;
+using namespace dev;
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const* _data, size_t _size)
+{
+	// Solidity program
+	string code(reinterpret_cast<char const*>(_data), _size);
+	// Solidity runtime bytecode
+	dev::bytes byteCode;
+	// Hex encoded runtime bytecode
+	std::string hexEncodedRuntimeCode;
+	// Hex encoded input
+	std::string hexEncodedInput;
+
+	// If we cannot compile contract, we simply bail.
+	try
+	{
+		SolidityExecutionFramework executionFramework;
+		byteCode = executionFramework.compileContract(code);
+		Json::Value methodIdentifiers = executionFramework.getMethodIdentifiers();
+
+		// Call the first function
+	    for (auto const& id : methodIdentifiers.getMemberNames())
+	    {
+		    hexEncodedInput = methodIdentifiers[id].asString();
+		    break;
+	    }
+	}
+	catch (...)
+	{
+		return 0;
+	}
+
+	hexEncodedRuntimeCode = toHex(byteCode);
+	return 0;
+}
---
+++ test/tools/ossfuzz/abiV2FuzzerCommon.cpp
@@ -0,0 +1,13 @@
+#include <test/tools/ossfuzz/abiV2FuzzerCommon.h>
+
+using namespace dev::test::abiv2fuzzer;
+
+SolidityExecutionFramework::SolidityExecutionFramework(langutil::EVMVersion _evmVersion)
+{
+	m_evmVersion = _evmVersion;
+}
+
+SolidityExecutionFramework::SolidityExecutionFramework()
+{
+	m_evmVersion = langutil::EVMVersion();
+}
